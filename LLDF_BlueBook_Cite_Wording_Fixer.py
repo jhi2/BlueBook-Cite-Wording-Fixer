534b5c2ce793802be79bc2256fc334e566c664da
jhi2 <johndrostan@outlook.com>
2024-02-28 16:37:29 -0800

    Add project files.

---
+++ LLDF_BlueBook_Cite_Wording_Fixer.py
@@ -0,0 +1,24 @@
+print("JohnnyTech LLDF BlueBook Cite Wording Fixer - running imports...")
+import csv
+import tkinter as tk
+from tkinter import ttk
+import urllib
+print("Definiiing varibles...")
+spacewords = ["District of Columbia","Los Angeles","New York","San Francisco"]
+def readconfig():
+    reader = csv.DictReader(open('BlueBook.csv'))
+    for row in reader:
+        return
+print("Reading configuration table...")
+config = readconfig()
+print("Initalizing GUI...")
+window = tk.Tk()
+print("Initalizing GUI elements...")
+header = ttk.Label(window,text="JohnnyTech LLDF BlueBook Cite Wording Fixer")
+brokencite = tk.Text(window,text="Enter your bad cite here...")
+print("Definiiing varibles...")
+def fixcite():
+    
+fixbutton = ttk.Button(window,text="Fix this cite")
+
+window.mainloop()
